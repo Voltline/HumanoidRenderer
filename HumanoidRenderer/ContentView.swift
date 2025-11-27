2e38a1ecf3ab73acdaaa7dc533938bf7b6ce2d3b
Voltline <44248193@qq.com>
2025-11-27 16:07:00 +0800

    feat: add some test code

--- HumanoidRenderer/ContentView.swift
+++ HumanoidRenderer/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                 .padding(.bottom, 50)
 
             Text("Hello, world!")
-
+            Text("Hello, Humanoid Renderer!")
             ToggleImmersiveSpaceButton()
         }
         .padding()
