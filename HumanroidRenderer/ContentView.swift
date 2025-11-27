5788ef834af4c31a5c48f877ef66aae0cbfe66a5
Voltline <44248193@qq.com>
2025-11-27 15:18:13 +0800

    Initial Commit

---
+++ HumanroidRenderer/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  HumanroidRenderer
+//
+//  Created by 张艺怀 on 2025/11/27.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ HumanroidRenderer/HumanroidRendererApp.swift
@@ -0,0 +1,33 @@
+//
+//  HumanroidRendererApp.swift
+//  HumanroidRenderer
+//
+//  Created by 张艺怀 on 2025/11/27.
+//
+
+import SwiftUI
+
+@main
+struct HumanroidRendererApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
